18d50f2910453e31876d3ba7226a3dc6522c066d
yanpilip <yanpilippp@gmail.com>
2026-04-15 17:58:54 +0200

    First commit. Fixed the ClassDefaults variable not working

--- Plugins/ThesisPlugin/Source/ThesisPlugin/Private/ThesisAnimInstance.cpp
+++ Plugins/ThesisPlugin/Source/ThesisPlugin/Private/ThesisAnimInstance.cpp
@@ -39,7 +39,7 @@ void UThesisAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 	{
 		return;
 	}
-	if (EnableFootIK) {
+	if (EnableFootIK && (GetSkelMeshComponent()->GetAnimInstance() == this)) {
 		if (!IsFalling) {
 			UpdateFootIKC(DeltaSeconds);
 		}
